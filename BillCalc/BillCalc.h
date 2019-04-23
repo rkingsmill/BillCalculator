bcb06645bfe99b24c66d5da7bbc5a16b25b205ef
Tayson Nguyen <tnguyen@touchbistro.com>
2019-04-23 13:43:23 -0400

    Initial Commit

---
+++ BillCalc/BillCalc.h
@@ -0,0 +1,19 @@
+//
+//  BillCalc.h
+//  BillCalc
+//
+//  Created by Tayson Nguyen on 2019-04-23.
+//  Copyright © 2019 TouchBistro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BillCalc.
+FOUNDATION_EXPORT double BillCalcVersionNumber;
+
+//! Project version string for BillCalc.
+FOUNDATION_EXPORT const unsigned char BillCalcVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BillCalc/PublicHeader.h>
+
+
